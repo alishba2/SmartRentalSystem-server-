369b7ab270fd401f52e99d86704adcbf2a487921
alishba2 <alishbashazadi79@gmail.com>
2024-08-03 17:57:48 +0500

    added atlas url

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-// db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    console.log("in db file");
-    await mongoose.connect('mongodb://127.0.0.1/SmartRentalSystem', {
+    console.log("In db file");
+
+    await mongoose.connect('mongodb+srv://alishba2:%2321alishba%2A@cluster0.9gntabz.mongodb.net/leaseConnect?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    
+
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
